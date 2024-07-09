94a5a20eb54ceec84c8a12e0da945ce519bc71d8
unknown <ahmedsm453@gmail.com>
2024-07-09 14:25:18 -0700

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+//Module -encapsulated code
+const names  = require('./4-name');
+const sayHi = require('./5-utile');
+const data = require('./6-alternative-flavour')
+require('./7-mind-grenade')
+
+sayHi('susan');
+sayHi(names.john);
+sayHi(names.peter);
